a300efc80448653dc00a9aa965b0ce4b9bb34615
Valentin <valentin.bogulean@student.upt.ro>
2023-10-30 16:53:59 +0200

    gitignore update 5

--- build/CMakeFiles/project_paoo_vb.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for project_paoo_vb.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/whelentyn/Desktop/lab_paoo/project_paoo_vb
-# Build directory: /home/whelentyn/Desktop/lab_paoo/project_paoo_vb/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
