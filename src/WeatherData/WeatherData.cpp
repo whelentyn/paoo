2ed74c461608740de459a8dc9f381424aba31c71
Valentin <valentin.bogulean@student.upt.ro>
2023-10-30 18:24:57 +0200

    implementation of WeatherData class and file structuring

--- src/WeatherData/WeatherData.cpp
+++ src/WeatherData/WeatherData.cpp
@@ -10,15 +10,41 @@ class WeatherData {
         char* description;
 
     public:
+        //constructor
         WeatherData(double temp, double hum, double press, const char* desc) :
             temperature(temp), humidity(hum), pressure(press) {
+                //allocate memory for data description and copy the string
                 description = new char[strlen(desc) + 1];
                 strcpy(description, desc);
             }
 
+        //copy constructor
         WeatherData(const WeatherData &other) :
             temperature(other.temperature), humidity(other.humidity), pressure(other.pressure) {
-                
+                description = new char[strlen(other.description) + 1];
+                strcpy(description, other.description);
             }
+
+        //destructor free dynamic memory
+        ~WeatherData() {
+            delete[] description;
+        }
+
+        double getTemperature() const {
+            return temperature;
+        }
+
+
+        double getHumidity() const {
+            return humidity;
+        }
+
+        double getPressure() const {
+            return pressure;
+        }
+
+        const char* getDescription() const {
+            return description;
+        }
 
 };
